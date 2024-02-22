0cc9808c7352986a89f5b53c1a34fe1791c14374
Tornike Zoidze <tornike.zoidze@gmail.com>
2024-02-22 19:49:46 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log('Hello World?');
+
+console.log(5 + 5);
+
+console.log(5 - 5);
+
+console.log( 5 + 5, 100 / 2);
+
+console.log ( (5 + 3) * 2 - 10);
+
+let x = 5 + 3;
+
+console.log (x * 2);
+
+let TornikesAge = 27;
+console.log(TornikesAge);
+console.log(TornikesAge + 200);
+
+letTotalCourseMonths = 7;
+let monthsPassed = 2;
+let monthsLeft = letTotalCourseMonths - monthsPassed;
+console.log (monthsLeft)
+
+
+
+F = 86;
+let C = (F - 32) * (5 / 9);
+
+console.log (C);
+
+Ce = 30;
+let Fa = Ce * (9 / 5) + 32 ;
+console.log (Fa);
+
+
+
+
